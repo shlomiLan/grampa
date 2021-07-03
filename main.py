f384431c5362853e5c6d5ec2dc5aa62b8b6473c7
shlomiLan <shlomilanton@gmail.com>
2021-07-03 22:38:28 +0300

    fix table - 2

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def run():
     all_files = sorted(all_files, key=lambda x: x[1], reverse=True)
 
     # print(tabulate(all_files, headers=["File name", "Last changed"], tablefmt="grid"))
-    return tabulate(all_files, headers=["File name", "Last changed"], tablefmt="grid")
+    print(tabulate(all_files[:10], headers=["File name", "Last changed"], tablefmt="grid"))
     # print(tabulate(all_files, headers=["File name", "Last changed"]))
 
 
